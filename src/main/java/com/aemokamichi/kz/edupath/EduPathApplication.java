0e7bef6f4b45932206f4577a9cb11be5e3ecfca6
Asemokamichi <asemokamichi@gmail.com>
2024-05-23 14:08:53 +0500

    first commit

---
+++ src/main/java/com/aemokamichi/kz/edupath/EduPathApplication.java
@@ -0,0 +1,13 @@
+package com.aemokamichi.kz.edupath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aemokamichi/kz/edupath/EduPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aemokamichi.kz.edupath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
